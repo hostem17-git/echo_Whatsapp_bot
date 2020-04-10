b688a9c57d8e22a60e77f6fef497a9d94e14f3b7
hostem17-git <vjjais17@gmail.com>
2020-04-11 03:58:27 +0530

    forgot to conver count to int

--- app.py
+++ app.py
@@ -31,15 +31,15 @@ def sms_reply():
         death= stat[3]
         rec=stat[5]
         c=c-1
-        t=0
-        t+=int(count)
+        #t=0
+        #t+=int(count)
        # if(c>=0):
          #   s +=  "--> " + country + "\n" +" total  "+ count + "\n"  + "death " + death+ "\n" + "rec " + rec + " \n <--"
           
         if( country=="India" ):
             s+= "--> " + country +"\n" +"total "+ count +"\n" +"death "+ death+"\n" +"rec "+ rec +"\n <--"
          
-        s+=" TOTAL "   +t + "\n"
+        #s+=" TOTAL "   +t + "\n"
          
     resp = MessagingResponse()
     resp.message(s)
